a3acf18b033eb6498c58d529d9078157bf6571f1
yedhink <yedhin1998@gmail.com>
2020-02-04 15:53:04 +0530

    add basic channels in goroutines

---
+++ goroutiness/channels.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func double(ip,op chan int){
+	for true {
+		num := <-ip
+		op <- num * 2
+	}
+}
+
+func main() {
+	to := make(chan int)
+	from := make(chan int)
+	go double(to,from)
+	for i:=0;i<5;i++ {
+		to <- i
+		fmt.Printf("%d\t%d\n",i,<-from)
+	}
+}
